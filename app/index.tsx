0e646a7f9901e9bd74e5c0f38bbc1ad6f89c6587
kobataAyaka <arka.cigarette@gmail.com>
2025-05-09 17:57:26 +0900

    reset

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+          <Text>안녕 Expo! 첫 프로젝트다. </Text>
+    </View>
+  );
+}
