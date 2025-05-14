d2ff87f3d35826ec7f4173ec7ed2a5e8be62dce2
kobataAyaka <arka.cigarette@gmail.com>
2025-05-14 16:59:25 +0900

    단어 입력 기능 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function Index() {
+    const [word, setWord] = useState('');
+const [words, setWords] = useState<string[]>([]);
+
+const addWord = () => {
+  if (word.trim() !== '') {
+    setWords([...words, word]);
+    setWord('');
+  }
+}
+
+const removeWord = (index: number) => {
+  const newWords = [...words];
+  newWords.splice(index, 1);
+  setWords(newWords);
+}
+
   return (
     <View style={styles.container}>
-      <Text>어서 오세요~!!</Text>
+      {/* <Text>어서 오세요~!!</Text> */}
+       <TextInput
+          placeholder="새로은 단어를 입력하세요"
+          value={word}
+          onChangeText={(text) => setWord(text)}
+            style={{ borderWidth: 1, width: 200, marginBottom: 10, padding: 5 }}
+    />
+        <Button title="추가" onPress={addWord} />
+        <FlatList
+            data={words || []} // words가 undefined인 경우 빈 배열을 넘기기
+            renderItem={({ item, index }) => (
+            <View>
+                <Text>{item}</Text>
+                <Button title="삭제" onPress={() => removeWord(index)} />
+            </View>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+        />
+
     </View>
   );
 }
